fff0e257d054119bc408f056a9959df7d17fc283
Taylor Hartsfield <taylormhartsfield@gmail.com>
2022-10-18 14:06:31 -0600

    Day 4 Part 1 Complete

---
+++ Day4/day4.py
@@ -0,0 +1,49 @@
+file = open('input.txt', 'r')
+lines= file.read().split('\n\n')
+_draws, *_boards = lines
+
+boards = [[list(map(int, row.split())) for row in board.split('\n')]
+                    for board in _boards]
+draws = list(map(int, _draws.split(',')))
+
+class Board:
+
+    def __init__(self,board): 
+
+        self.board = board
+        self.rows = [set(row) for row in board]
+        self.cols = [set(col) for col in zip(*board)]
+    
+    def check_for_match(self, num):
+
+        for row in self.rows:
+            if num in row:
+                row.remove(num)
+
+        for col in self.cols:
+            if num in col:
+                col.remove(num)
+
+        return self.check_for_bingo_rows(num) or self.check_for_bingo_cols(num)
+
+    def check_for_bingo_cols(self,num):
+        if any(len(col) == 0 for col in self.cols):
+            return sum(list(map(sum, self.cols))) * num
+    
+    def check_for_bingo_rows(self, num):
+        if any(len(row) == 0 for row in self.rows):
+            return sum(list(map(sum, self.rows))) * num
+    
+boards = [Board(board=board) for board in boards]
+
+
+def play_game():
+    for draw in draws:
+        for board in boards:
+            score = board.check_for_match(draw)
+            if score:
+                return score
+
+
+#Part 1 // CORRECT ⭐️
+print(f'PART 1: {play_game()}')
